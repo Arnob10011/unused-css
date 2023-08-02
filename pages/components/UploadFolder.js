a90c328c4ccd934b035999b2dae8f73db442c21e
Arnob <87138850+Arnob10011@users.noreply.github.com>
2023-08-02 17:07:22 +0600

    Update UploadFolder.js

--- pages/components/UploadFolder.js
+++ pages/components/UploadFolder.js
@@ -4,85 +4,96 @@ import axios from 'axios'
 import {useDropzone} from 'react-dropzone'
 import { useCallback, useState, useEffect} from 'react'
 import Link from 'next/link'
+
+
 export default function UploadFolder() {
-
-
-    const readFiles = (files) => {
-        return new Promise((resolve, reject) => {
-          const fileInfos = [];
-          let filesProcessed = 0;
-      
-          const handleLoad = (file, content) => {
-            fileInfos.push({
-              path: file.path,
-              name: file.name,
-              size: file.size,
-              type: file.type,
-              content: content,
-            });
-      
-            filesProcessed++;
-      
-            if (filesProcessed === files.length) {
-              resolve(fileInfos);
-            }
-          };
-      
-          files.forEach((file) => {
-            const fileReader = new FileReader();
-      
-            fileReader.onload = (e) => {
-              handleLoad(file, e.target.result);
-            };
-      
-            fileReader.onerror = (e) => {
-              reject(e);
-            };
-      
-            fileReader.readAsText(file);
-          });
-        });
+  
+  // reads file 
+  async function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onload = (e) => {
+        resolve(e.target.result);
       };
+  
+      reader.onerror = (e) => {
+        reject(e);
+      };
+  
+      reader.readAsText(file);
+    });
+  }
+
+
+  async function readFileContents(files){
+
+    const fileInfo = [];
+
+    const fileReadingPromises = files.map(async (file) => {
+      const largeFiles = file.path.includes('node_modules');
+      if (!largeFiles) {
+        const content = await readFileAsync(file);
+        fileInfo.push({
+          path: file.path,
+          name: file.name,
+          size: file.size,
+          type: file.type,
+          content: content,
+        });
+      }
+    });
+  
+    await Promise.all(fileReadingPromises);
+    return fileInfo
+
+  }
+
+
+
+
+
+  const onDrop = useCallback(async (files) => {
+    try {
+
+      const filesInfo = await readFileContents(files)
+      console.log(filesInfo) 
+
+     
+
     
-    
-    
-    
-    
-    
-    
-      const onDrop = useCallback(async (files) =>  {
-        const fileInfo = await readFiles(files)
-    
-       await axios.post('http://localhost:5000/api/uploadfiles', fileInfo, {
+      await axios.post('http://localhost:5000/api/uploadfiles', filesInfo, {
         headers: {
           'Content-Type': 'application/json'
+
         }
-       })
       })
-      const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-    
-      return (
-       <>
-       
-       <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
-          <input {...getInputProps()} />
-          {
-            isDragActive ?
-            <p>Drop the files here...</p> :
-            <p>Drag and drop files here, or click to select files</p>
-          }
-        </div>
-    
-        <div>
+
+    } catch (error) {
+      console.error('Error reading files:', error);
+    }
+  }, []);
+
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <>
+      <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here...</p>
+        ) : (
+          <p>Drag and drop files here, or click to select files</p>
+        )}
+      </div>
+
+      <div>
+  
         <Link href="http://localhost:5000/api/downloadcleanedcss">
-          Download 
+          Download
         </Link>
-        </div>
-    
-    
-    
-    
-       
-       </>
-      )
+      </div>
+    </>
+  );
 }
