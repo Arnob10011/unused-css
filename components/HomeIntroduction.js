ea4fb69b6f4f444d6e00f3b4ee7a0658d2879d05
Arnob <87138850+Arnob10011@users.noreply.github.com>
2023-08-06 16:17:53 +0600

    update components

---
+++ components/HomeIntroduction.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function HomeIntroduction() {
+  return (
+    <div>
+                <h1 className='text-6xl font-bold text-center mb-5 text-white'>
+                  Remove <span className='text-slate-900'>Unused</span>{" "}
+                  Stylesheet
+                </h1>
+                <p className='text-[20px] text-center text-slate-200 mb-10'>
+                  Used by some of the world's largest companies, Next.js enables
+                  you to create full-stack Web applications by extending the
+                  latest React features, and integrating powerful Rust-based
+                  JavaScript tooling for the fastest builds.
+                </p>
+              </div>
+  )
+}
---
+++ components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SpinnerIcon from '@/components/SVG/SpinnerIcon'
+export default function page() {
+  return (
+    <div className='bg-svg flex justify-center items-center h-full'>
+
+    <SpinnerIcon/>
+      
+    </div>
+  )
+}
